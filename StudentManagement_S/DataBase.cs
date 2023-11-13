0f6e19e80316b2bec6a00f21958feb0a0f682685
Ranga Dananjaya <rangamaithreepala@gmail.com>
2023-11-14 03:31:51 +0530

    first commit

---
+++ StudentManagement_S/DataBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement_S
+{
+    public class DataBase : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+
+        public DbSet<Student> Students { get; set; }
+
+        public readonly string _path = @"D:\Cmps doc\Campus lec\3rd sem\EE3250 GUI Programming\Assignment\Group Project\Student Management Sys\StudentManagement_S\data.db";
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source = {_path}");
+        }
+    }
+}
